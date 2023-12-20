6cf506a8ebfd705624a0073de9de8fe5e3d00c50
Sanket Garde <sanketgarde7473@gmail.com>
2023-12-20 14:49:50 +0530

    Ability to create a Address book table

--- Address_book_system_sql.sql
+++ Address_book_system_sql.sql
@@ -4,3 +4,18 @@ Welcome to address book system
 create database Address_Book_Service;
 show databases;
 use address_book_service;
+
+//UC2 Create a Address_Book_table with differen attributes
+
+create table Address_Book_Table
+(
+firstname varchar(255) not null,
+lastname varchar(255) not null,
+address varchar(255) not null,
+city varchar(255),
+state varchar (255),
+zip int,
+phone_number varchar(20),
+email varchar(255)
+
+);
